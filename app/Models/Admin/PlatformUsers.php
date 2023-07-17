b9865bb78e614d142dc13960d98dfd011240f54b
zhengyuying <1067652489@qq.com>
2023-07-17 17:48:28 +0800

    test

--- app/Models/Admin/PlatformUsers.php
+++ app/Models/Admin/PlatformUsers.php
@@ -4,13 +4,19 @@
 namespace App\Models\Admin;
 
 use Encore\Admin\Auth\Database\Administrator;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class PlatformUsers extends Administrator
 {
     protected $fillable = ['platform_id','username', 'password', 'name', 'avatar'];
 
-    public function platform()
+    /**
+     * @return BelongsTo
+     */
+    public function platform() : BelongsTo
     {
-        return $this->belongsTo(Platform::class);
+        $platformModel = config('admin.database.platforms_model');
+        return $this->belongsTo($platformModel,'platform_id','id');
     }
 }
