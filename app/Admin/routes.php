047b6739f7e6a4487fceb71cafd520451215cc51
zhengyuying <1067652489@qq.com>
2023-07-27 15:38:56 +0800

    test

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -30,7 +30,7 @@ Route::group([
             //文件资源
             $router->resource('sources','SourceController');
             //文件模板
-            $router->group(['prefix' => 'templates','namespace' => 'Template'],function() use ($router) {
+            $router->group(['prefix' => 'templates'],function() use ($router) {
                 $router->resource('/', 'TemplateController');
                 $router->match(['get', 'post'], 'import_page', 'TemplateController@importPage')->name('material_file_templates_import_page');
                 // 模板导入
