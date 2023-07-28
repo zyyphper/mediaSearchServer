b281259f50b34273050bd5e078e52cab9d6c27b4
zhengyuying <1067652489@qq.com>
2023-07-28 12:42:48 +0800

    test

--- app/Admin/Controllers/Vip/LevelController.php
+++ app/Admin/Controllers/Vip/LevelController.php
@@ -76,20 +76,18 @@ class LevelController extends BaseAdminController
             $form->text('name', '等级描述')->rules(['required|max:32']);
             $form->number('requirement_score', '等级达标分数')->rules(['required|min:1']);
         })->tab('等级权益',function (Form $form) {
-            $form->hasMany('levelEquities','权益',function (Form\NestedForm $form) {
-               $form->radio('equity_id','权益')->options(
-                   [1=>'空间存储',2=>'文件检索',3=>'类型转换']
-//                   VipEquity::all()->pluck('name','id')
-               )->when(1,function (Form $form) {
-                        $form->number('num','数值');
+            $form->radio('equity_id','权益')->options(VipEquity::all()->pluck('name','id'))->when(VipEquity::SPACE_CAPACITY,function (Form $form) {
+                $form->number('num','数值');
 //                        $form->radio('unit','单位')->options(EquityUnit::load(EquityUnit::SPACE_CAPACITY)->pluck());
-                   })->when(VipEquity::FILE_SEARCH,function (Form $form) {
-                       $form->number('num','数值');
+            })->when(VipEquity::FILE_SEARCH,function (Form $form) {
+                $form->number('num','数值');
 //                       $form->radio('unit','单位')->options(EquityUnit::load(EquityUnit::FILE_SEARCH)->pluck());
-                   })->when(VipEquity::TYPE_CONVERT,function (Form $form) {
-                       $form->number('num','数值');
+            })->when(VipEquity::TYPE_CONVERT,function (Form $form) {
+                $form->number('num','数值');
 //                       $form->radio('unit','单位')->options(EquityUnit::load(EquityUnit::TYPE_CONVERT)->pluck());
-                   });
+            });
+            $form->hasMany('levelEquities','权益',function (Form\NestedForm $form) {
+
             });
         });
 
